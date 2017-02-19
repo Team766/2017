d7582817f47c862f9dd54357a68b4605efd7c32f
blevenson <blevenson68@gmail.com>
2017-02-18 17:01:58 -0800

    Drive with velocity controller

--- src/com/team766/robot/Actors/Auton/AutonSelector.java
+++ src/com/team766/robot/Actors/Auton/AutonSelector.java
@@ -35,7 +35,7 @@ public class AutonSelector extends Actor{
 			case "None":
 				System.out.println("Auton: None");
 				LogFactory.getInstance("General").print("Auton: None");
-				waitForMessage(new DriveDistance(4, 180), DriveStatusUpdate.class);
+//				waitForMessage(new DriveDistance(4, 180), DriveStatusUpdate.class);
 				waitForMessage(new DriveDistance(4, 0), DriveStatusUpdate.class);
 				break;
 			case "DriveToPeg":
