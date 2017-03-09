21a5ce1b2d067100c320c2c4b4caa1786479d109
ZhanningZ <796821@seq.org>
2017-03-08 19:01:00 -0800

    fix climber test

--- tests/com/team766/tests/ClimberTest.java
+++ tests/com/team766/tests/ClimberTest.java
@@ -1,24 +1,42 @@
 package com.team766.tests;
 
+import lib.Message;
 import lib.Scheduler;
 
 import com.team766.lib.ConfigFile;
+import com.team766.lib.Messages.Stop;
 import com.team766.lib.Messages.UpdateClimber;
+import com.team766.lib.Messages.ClimbDeploy;
+import com.team766.robot.Constants;
+
 import tests.RobotTestCase;
 
 public class ClimberTest extends RobotTestCase{
 	
 	public void testClimberMotor() throws Exception{
-		//True
-		Scheduler.getInstance().sendMessage(new UpdateClimber(true));
-		//Check climber motor moving
-		assertTrueTimed(() -> {return instance.getMotor(ConfigFile.getClimberMotor()).get() > 0;}, 2);
 		
+		//Test updateClimber
+		//true
+		Scheduler.getInstance().sendMessage(new UpdateClimber(true));
+		//Check climber motor positive
+		assertTrueTimed(() -> {return instance.getMotor(ConfigFile.getClimberMotor()).get() > 0;}, 2);
 		
 		//false
 		Scheduler.getInstance().sendMessage(new UpdateClimber(false));
-		//Check climber motor stop
-		assertTrueTimed(() -> {return instance.getMotor(ConfigFile.getClimberMotor()).get() == 0;}, 2);
+		//Check climber motor negative
+		assertTrueTimed(() -> {return instance.getMotor(ConfigFile.getClimberMotor()).get() < 0;}, 2);
+		
+		
+		//Test climberDeploy
+		//true
+		Scheduler.getInstance().sendMessage(new ClimbDeploy(true));
+		//Check climberSolenoid is true
+		assertTrueTimed(() -> {return instance.getSolenoid(ConfigFile.getClimberDeploy()).get() == true;}, 2);
+		
+		//false
+		Scheduler.getInstance().sendMessage(new ClimbDeploy(false));
+		//Check climberSolenoid is false
+		assertTrueTimed(() -> {return instance.getSolenoid(ConfigFile.getClimberDeploy()).get() == false;}, 2);	
 		
 	}
 
